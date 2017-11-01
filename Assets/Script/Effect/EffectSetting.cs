b0c6060b43f37fb5b2d45e02f5f64d6aabe36f67
elvis992035 <elvis992035@gmail.com>
2017-10-31 22:54:15 -0400

    Initial commit

---
+++ Assets/Script/Effect/EffectSetting.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectSetting : MonoBehaviour {
+
+	public enum EffectTypeEnum
+	{
+	   Projectile,
+	   AOE,
+	   Other
+	};
+
+	public enum DeactivationEnum
+	{
+       Deactivate,
+	   DesstroyAfterCollision,
+	   DestroyAfterTime,
+	   Nothing
+	};
+
+	public EffectTypeEnum EffectType; 
+
+	public float ColliderRadius = 0.2f;
+
+	public float EffectRadius = 0;
+
+	// Get the position of the movement of the notion vector, and not to follow to the target.
+	public bool UseMoveVector;  
+
+	public GameObject Target;
+
+	public Vector3 MoveVector = Vector3.forward;
+
+	public float MoveSpeed = 1;
+
+	public bool IsHomeingMove;
+
+	public float MoveDistance = 20;
+
+	public bool IsVisible = true;
+
+	public DeactivationEnum InstanceBehaviour = DeactivationEnum.Nothing;
+
+	public float DeactivateTimeDelay = 4;
+
+	public float DestroyTimeDelay = 10;
+
+	public LayerMask LayerMask = -1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position += MoveVector;
+	}
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private EffectSetting effectSetting;
+
+    public GameObject Camera;
+
+    private Vector3 offset;
+
+	public GameObject Target;
+
+	public GameObject[] Magic;
+
+	private float MagicSpeed = 4;
+
+    private void OnGUI()
+    {
+        
+    }
+    // Use this for initialization
+    void Start () {
+		
+        offset = Camera.transform.position - transform.position;  // distance between char and camera
+
+		var magicSet = Magic [0].GetComponent<EffectSetting> ();
+		MagicSpeed = magicSet.MoveSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 200.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 8.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+		if (Input.GetMouseButton (0)) {
+
+			isFire ();
+			Debug.Log (Target.transform.position);
+		}
+
+
+    }
+
+	void isFire(){
+		
+		GameObject magic = Instantiate (Magic [0], transform.position, Quaternion.identity) as GameObject;
+
+	}
+
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        Camera.transform.position = transform.position + offset;
+    }
+}
